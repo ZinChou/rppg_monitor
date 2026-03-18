12fbda688d69c2d2998633a4c748f22b5ae6b1de
ZinChou <1546250299@qq.com>
2026-03-18 20:54:44 +0800

    git push

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
-import time
-from collections import deque
-from model.POS import POS
-import cv2
-import numpy as np
-from model.POS import POS
+import multiprocessing as mp
+
 from Monitor import Monitor
 
 if __name__ == "__main__":
-    model = POS()
+    mp.freeze_support()
     monitor = Monitor(
-        model=model,
         camera_id=0,
         buffer_seconds=10,
         pos_window_seconds=1.6,
         display_scale=1.0,
+        target_fps=30.0,
     )
     monitor.run()
